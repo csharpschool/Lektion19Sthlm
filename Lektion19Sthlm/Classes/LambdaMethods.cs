722ad3c963f87669a940f86b71a0a7957f439932
Jonas Fagerberg <info@csharpschool.com>
2023-10-09 10:25:53 +0200

    Add project files.

---
+++ Lektion19Sthlm/Classes/LambdaMethods.cs
@@ -0,0 +1,36 @@
+﻿namespace Lektion19Sthlm.Classes;
+
+public class Book
+{
+    public int Id { get; init; }
+    public string Title { get; set; }
+
+    public Book(int id, string title) => (Id, Title) = (id, title);
+}
+
+public class LambdaMethods
+{
+    List<Book> Books { get; } = new();
+
+    public LambdaMethods()
+    {
+        Books.Add(new Book(1, "Dragon Ninja"));
+        Books.Add(new Book(2, "Shadow Walker"));
+        Books.Add(new Book(3, "Dragon Reborn"));
+    }
+
+    public List<Book> Get(Func<Book, bool>? lambda)
+    {
+        try
+        {
+            return lambda is null ? Books : Books.Where(lambda).ToList();
+
+            /*if (lambda is null) return Books;
+            return Books.Where(lambda).ToList();*/
+        }
+        catch
+        {
+            return new List<Book>();
+        }
+    }
+}
---
+++ Lektion19Sthlm/Program.cs
@@ -0,0 +1,13 @@
+using Lektion19Sthlm;
+using Lektion19Sthlm.Classes;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton<LambdaMethods>();
+
+await builder.Build().RunAsync();
